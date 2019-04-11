a6fb6f5ec853c858e2d9e83ab6403eab4398b8ed
estsan <sandstrom.ester@gmail.com>
2019-04-11 18:46:02 +0200

    Lite mer uppfrächning.

--- Geocaching/PersonDialog.xaml.cs
+++ Geocaching/PersonDialog.xaml.cs
@@ -5,18 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-// Att göra: När man klickar på add geocache och ingen person är vald ska felmeddelande dyka upp. Annars ska geocachen ha den personen som person.
-
-
-
-
 
 namespace Geocaching
 {
